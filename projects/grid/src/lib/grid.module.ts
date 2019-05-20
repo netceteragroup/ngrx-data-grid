411f3d8820fa6efa6eb14eec3abed927afa08f7a
Ignatij Gichevski <Ignatij.Gichevski@netcetera.com>
2019-05-20 16:05:12 +0200

    [RNE014-2859] Create a new Angular project in the PCS repo
    Add ReduxDevTools.

--- projects/grid/src/lib/grid.module.ts
+++ projects/grid/src/lib/grid.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { GridExampleModule } from '@example/grid-example.module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   imports: [
-    GridExampleModule
+    GridExampleModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 30
+    })
   ],
   exports: [
     GridExampleModule
