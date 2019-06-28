4c6955d262621495a132c60009b0905f64bf19ba
Martin Vrencovski <Martin.Vrencovski@netcetera.com>
2019-06-28 10:39:03 +0200

    [RNE014-2854] Checkbox Row Selction new selector

--- projects/grid/src/lib/store/index.ts
+++ projects/grid/src/lib/store/index.ts
@@ -1,6 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { gridReducer, GridState } from './grid-reducer';
 import { GridConfig } from '@grid/config/grid-config';
+import * as R from 'ramda';
 
 // root state
 export interface State {
@@ -39,7 +40,7 @@ export const getPagedData = createSelector(
   (state: GridState) => state.pagedData
 );
 
-export const getSelectedRows = createSelector(
+export const getSelectedRowIndexes = createSelector(
   getGridConfig,
   (config: GridConfig) => config.selectedRowsIndexes
 );
@@ -47,4 +48,10 @@ export const getSelectedRows = createSelector(
 export const getNumberOfRows = createSelector(
   getGridState,
   (state: GridState) => state.gridData.length
+);
+
+export const getSelectedRows = createSelector(
+  getGridData,
+  getSelectedRowIndexes,
+  (data: Array<Object>, indexes: Array<any>) => R.filter(R.compose(R.flip(R.contains)(indexes), R.prop('gridRowId')), data)
 );
