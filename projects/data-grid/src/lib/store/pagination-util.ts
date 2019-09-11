86b5072a083e1a8be40cf4e24ed38e7e31cdfdaa
Hristijan Dimitrieski <hristijan.dimitrieski@gmail.com>
2019-09-11 09:56:43 +0200

    feat(custom-filters): remove unused function

--- projects/data-grid/src/lib/store/pagination-util.ts
+++ projects/data-grid/src/lib/store/pagination-util.ts
@@ -2,8 +2,6 @@ import * as R from 'ramda';
 
 export const calculateNumberOfPages = (dataSize: number, paginationPageSize: number): number => Math.ceil(dataSize / paginationPageSize);
 
-export const sizeOfShownData = (dataSize: number, pageSize: number): number => R.max(dataSize, pageSize);
-
 export const getPagedData = <T>(data: T[], currentPage: number, pageSize: number) => {
   const dataSize = data.length;
   const startIndex = currentPage === 0 ? 0 : (currentPage * pageSize);
