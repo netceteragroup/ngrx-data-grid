5130e83cc5a2723c32e2ed114956fe635601c2a0
Hristijan Dimitrieski <Hristijan.Dimitrieski@netcetera.com>
2019-10-31 14:39:10 +0100

    feat(grid): implement dynamic column width

--- projects/data-grid/src/lib/components/grid-display.component.ts
+++ projects/data-grid/src/lib/components/grid-display.component.ts
@@ -1,8 +1,16 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges
+} from '@angular/core';
 import * as R from 'ramda';
 import { SelectionConfig } from '../config';
 import { ApplyFilterEvent, DataGridColumnWithId, GridDataSortWithColumnId } from '../models';
-import { getNumberOfVisibleColumns } from '../util/grid-columns';
+import { ColumnsStyle, toColumnsStyle } from '../util/columns-style';
 
 @Component({
   selector: 'ngrx-grid-display',
@@ -10,7 +18,7 @@ import { getNumberOfVisibleColumns } from '../util/grid-columns';
   styleUrls: ['grid-display.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GridDisplayComponent {
+export class GridDisplayComponent implements OnChanges {
   @Input() selectionConfig: SelectionConfig;
   @Input() columns: DataGridColumnWithId[] = [];
   @Input() gridRows: any[] = [];
@@ -23,14 +31,14 @@ export class GridDisplayComponent {
   @Output() toggleSelectAllRows = new EventEmitter();
   @Output() toggleRow = new EventEmitter();
 
-  get gridColumns() {
-    const columnsWidth = R.join(' ', R.map(({width}) => width ? this.getWidth(width)  : 'minmax(150px, 1.4fr)', this.columns));
-    const selection = (this.selectionConfig.checkboxSelection) ? '3rem ' : '';
-    return {'grid-template-columns': `${selection} ${columnsWidth}`};
-  }
+  columnsStyle: ColumnsStyle;
 
-  getWidth(width: number) {
-    return width < 200 ? `${width}px` : `minmax(150px, ${width}px)`;
+  ngOnChanges({columns: columnsChanges}: SimpleChanges): void {
+    if (columnsChanges && columnsChanges.currentValue !== columnsChanges.previousValue) {
+      const columnsStyle = toColumnsStyle(this.columns);
+      const selectionStyle = (this.selectionConfig.checkboxSelection) ? '3rem' : '';
+      this.columnsStyle = {'grid-template-columns': `${selectionStyle} ${columnsStyle}`};
+    }
   }
 
   trackByIndex(_, index) {
---
+++ projects/data-grid/src/lib/util/columns-style.ts
@@ -0,0 +1,39 @@
+import * as R from 'ramda';
+import { getVisibleColumns } from './grid-columns';
+import { DataGridColumnWithId } from '../models';
+
+export interface ColumnsStyle {
+  'grid-template-columns': string;
+}
+
+const minmaxStyle = (width: string): string => `minmax(150px, ${width})`;
+
+const MIN_ALLOWED_WIDTH = 200;
+const DEFAULT_MAX_WIDTH = '1.4fr';
+const DEFAULT_WIDTH = minmaxStyle(DEFAULT_MAX_WIDTH);
+
+const isWidthAllowed = R.lt(MIN_ALLOWED_WIDTH);
+const inPixels = (width: number) => `${width}px`;
+const widthStyle = R.compose(minmaxStyle, inPixels);
+
+const toWidthInPixels = R.ifElse(
+  isWidthAllowed,
+  inPixels,
+  widthStyle
+);
+
+const hasWidth = R.has('width');
+const getWidth = R.prop('width');
+
+const toWidth = R.ifElse(
+  hasWidth,
+  R.compose(toWidthInPixels, getWidth),
+  R.always(DEFAULT_WIDTH)
+);
+
+type ToColumnsStyle = (columns: DataGridColumnWithId[]) => string;
+export const toColumnsStyle: ToColumnsStyle = R.compose(
+  R.join(' '),
+  R.map(toWidth),
+  getVisibleColumns
+);
